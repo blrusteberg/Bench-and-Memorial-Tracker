779c98ac3801f49877a5c7ab134a22bf2624cd7c
Swade Howard <swhowar@siue.edu>
2020-03-01 23:14:48 -0600

    Fixed API route causing errors

--- api/server.js
+++ api/server.js
@@ -5,29 +5,29 @@ const YAML = require('yamljs')
 const swaggerDocument = YAML.load('./swagger.yaml')
 const memorials = require('./data/memorials.json')
 
-app.use('/api/v1', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
     res.status(308)
-    res.redirect('/api/v1')
+    res.redirect('/api/docs')
 })
 
-app.get('/api/v1/memorials/trees', (req, res) => {
+app.get('/api/memorials/trees', (req, res) => {
     res.status(200)
     res.send(memorials.trees)
 })
 
-app.get('/api/v1/memorials/trees/{id}', (req, res) => {
+app.get('/api/memorials/trees/{id}', (req, res) => {
     res.status(200)
     res.send(getTreeById(req.params.id))
 })
 
-app.get('/api/v1/memorials/benches', (req, res) => {
+app.get('/api/memorials/benches', (req, res) => {
     res.status(200)
     res.send(memorials.benches)
 })
 
-app.get('/api/v1/memorials/benches/{id}', (req, res) => {
+app.get('/api/memorials/benches/{id}', (req, res) => {
     res.status(200)
     res.send(getBenchById(req.params.id))
 })
