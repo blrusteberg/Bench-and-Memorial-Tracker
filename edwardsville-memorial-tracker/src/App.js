ba81337929b1ac21c4de4f843466fc3895a9ac8f
Swade Howard <swhowar@siue.edu>
2020-03-19 12:40:26 -0500

    basic react map

---
+++ edwardsville-memorial-tracker/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import GoogleMap from "./GoogleMap/component";
+
+const data = {
+  
+}
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleMap/>
+    </div>
+  );
+}
+
+export default App;
