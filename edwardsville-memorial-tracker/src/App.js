5ceb934e11cb1b2becd69f0f88601b7a220022c6
Swade Howard <swhowar@siue.edu>
2020-03-19 22:04:42 -0500

    Added icons and search sidebar

--- edwardsville-memorial-tracker/src/App.js
+++ edwardsville-memorial-tracker/src/App.js
@@ -2,21 +2,36 @@ import React from "react";
 import "./App.css";
 
 import GoogleMap from "./GoogleMap/component";
-import Memorial from "./GoogleMap/Memorial";
+import Sidebar from "./Sidebar/component";
 
-const currentLocation = {lat: 38.806649, lng: -89.938928}
-const memorialData = {
-  "id":1,
-  "donator":"John Doe",
-  "x-coordinate":38.806649, 
-  "y-coordinate":-89.938928
-}
-
+const currentLocation = { lat: 38.812203, lng: -89.957655 };
+const memorialData = [
+  {
+    id: 1,
+    type: "Tree",
+    donator: "John Doe",
+    latitude: 38.806649,
+    longitude: -89.938928,
+    dateDonated: "1/2/2020"
+  },
+  {
+    id: 2,
+    type: "Bench",
+    donator: "Noah Dirk",
+    latitude: 38.810782,
+    longitude: -89.957078,
+    dateDonated: "1/3/2020"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <Memorial/>
+      <GoogleMap
+        memorialData={memorialData}
+        currentLocation={currentLocation}
+      />
+      <Sidebar memorialData={memorialData} />
     </div>
   );
 }
---
+++ edwardsville-memorial-tracker/src/Sidebar/component.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Memorial from "./Memorial";
+import Icon from "../GoogleMap/Icon";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+`;
+
+const Sidebar = ({ memorialData }) => {
+  return (
+    <div className="sidebar">
+      <div className="searchField">
+        <input type="text" placeholder="Search for a memorial" />
+        <Button>Search</Button>
+      </div>
+      <Memorial data={memorialData[0]} />
+    </div>
+  );
+};
+
+export default Sidebar;
