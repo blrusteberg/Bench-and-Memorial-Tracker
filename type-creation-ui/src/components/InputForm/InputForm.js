9778c1fae336c591a2e84077fb7815bc4db914b3
Celend <emcmast@siue.edu>
2020-04-09 12:08:22 -0500

    add delete delete button

--- type-creation-ui/src/components/InputForm/InputForm.js
+++ type-creation-ui/src/components/InputForm/InputForm.js
@@ -5,10 +5,14 @@ class InputForm extends React.Component {
       super(props);
       this.state = {
         selected: props.selected.attributes,
+        number: 0
     };
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      // this.handleSelected = this.handleSelected.bind(this);
+      this.addAttribute = this.addAttribute.bind(this);
+      this.deleteAttribute = this.deleteAttribute.bind(this);
     }
 
     handleChange(event) {
@@ -21,20 +25,41 @@ class InputForm extends React.Component {
   
     handleSubmit(event) {
     }
+
+    addAttribute(e) {
+      e.preventDefault();
+      let newAttribute = {};
+      this.setState({
+        selected: this.state.selected.concat(newAttribute)
+      });
+    }
+
+    deleteAttribute(attribute, e) {
+      e.preventDefault();
+      delete this.state.selected[attribute];
+      let filtered = this.state.selected.filter(function (el) {
+        return el != null;
+      });
+      this.setState({ selected : filtered });
+    }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            {this.state.selected.map((items) => (Object.keys(items)[0] === 'coordinates') ?
-              <div><input type="text" value='latitude' name={Object.keys(items)[0]} /> 
-              <br />
-              <input type="text" value='longitude'name={Object.keys(items)[0]} /></div> :
-              <div><input type="text" value={Object.keys(items)[0]} name={Object.keys(items)[0]} /></div>
-            )}
-          </label>
+        <div>
+          <button onClick={this.addAttribute}>Add Attribute</button>
           <input type="submit" value="Save" />
-        </form>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              {this.state.selected.map((items, n) => (Object.keys(items)[0] === 'coordinates') ?
+                <div><input type="text" defaultValue='latitude'/> 
+                <br />
+                <input type="text" defaultValue='longitude'/></div> :
+                <div><input type="text" key={Object.keys(items)[0]} defaultValue={Object.keys(items)[0]} />
+                <button onClick={this.deleteAttribute.bind(this, n)}>DELETE</button></div>
+              )}
+            </label>
+          </form>
+        </div>
       );
     }
 }
