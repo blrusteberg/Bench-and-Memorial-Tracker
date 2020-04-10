eec0b634f5c260d2d86df198885ed80d376d8049
Swade Howard <swhowar@siue.edu>
2020-04-10 15:48:22 -0500

    changed fetch to axios for api calls

--- memorial-tracker-ui/src/components/SideBar/Sidebar.js
+++ memorial-tracker-ui/src/components/SideBar/Sidebar.js
@@ -6,14 +6,16 @@ const sidebar = (props) => {
   return (
     <div className={styles.Sidebar}>
       <input
-        onChange={event => props.searchHandler(event.target.value)}
+        onChange={(event) => props.searchHandler(event.target.value)}
         className={styles.SearchInput}
         type="text"
         placeholder="What are you looking for?"
       />
       <div className={styles.ScrollMenu}>
         {props.memorials.map((m) => {
-          return <Memorial key={m.id} donator={m.donator} type={m.type} hide={m.hide}/>;
+          return (
+            <Memorial key={m.id} donor={m.donor} type={m.type} hide={m.hide} />
+          );
         })}
       </div>
     </div>
