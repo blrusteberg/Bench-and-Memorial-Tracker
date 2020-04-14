f3fdcd2e868cc6d77660ffaa7313bbafe2c4ad8e
blruste <blruste@siue.edu>
2020-04-14 13:29:52 -0500

    small fix

--- memorial-tracker-ui/src/containers/App.js
+++ memorial-tracker-ui/src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import "./App.css";
 import Map from "../components/Map/Map";
 import Sidebar from "../components/SideBar/Sidebar";
