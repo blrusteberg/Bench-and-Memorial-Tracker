3e9cb5e6e13b380ff67d49f2ef7da1ff8b208109
Swade Howard <swhowar@siue.edu>
2020-04-20 12:21:58 -0500

    added tagger form for user story

---
+++ admin-panel/src/components/Dash/TaggerForm/TaggerForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./TaggerForm.module.css";
+
+class TaggerForm extends React.Component(props) {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMound() {
+    console("api call here");
+  }
+
+  render() {
+    return {};
+  }
+}
+
+export default TaggerForm;
