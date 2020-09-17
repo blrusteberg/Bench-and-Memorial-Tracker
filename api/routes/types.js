15ca8d0dc61aa84740896ca695d0585742a410e1
Ethan McMasters <emcmast@siue.edu>
2020-09-17 12:01:27 -0500

    fix merge conflicts

--- api/routes/types.js
+++ api/routes/types.js
@@ -75,10 +75,6 @@ router.put("/:Id/attributes", async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-=======
-ls
->>>>>>> develop
 router.put("/:Id", async (req, res) => {
   try {
     const numUpdated = await Type.query().findById(req.params.Id).patch({
