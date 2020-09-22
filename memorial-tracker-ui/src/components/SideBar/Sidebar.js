51cdebdf7cd2c6b113c2641f61a9190ca774d0f1
Swade Howard <swade.howard@gmail.com>
2020-09-22 17:11:36 -0500

    Fixed memorial tracker UI to use new API routes/data

--- memorial-tracker-ui/src/components/SideBar/Sidebar.js
+++ memorial-tracker-ui/src/components/SideBar/Sidebar.js
@@ -1,30 +1,59 @@
 import React from "react";
+
+import { MapCenterContext } from "../../containers/App";
 import Memorial from "./Memorial/Memorial";
 import styles from "./Sidebar.module.css";
+import { getCoordinatesOfMemorial } from "../../utils/utils";
 
-const sidebar = (props) => {
-  return (
-    <div className={styles.Sidebar}>
-      <input
-        onChange={(event) => props.searchHandler(event.target.value)}
-        className={styles.SearchInput}
-        type="text"
-        placeholder="What are you looking for?"
-      />
-      <div className={styles.ScrollMenu}>
-        {props.memorials.map((m) => {
-          return (
-            <Memorial
-              key={m.guid}
-              attributes={m.attributes}
-              type={m.type}
-              hide={m.hideIcon}
-            />
-          );
-        })}
+class Sidebar extends React.Component {
+  state = {
+    lastClickedCoordinates: {
+      lat: 0,
+      lng: 0,
+    },
+  };
+
+  onSidebarClick = (memorialId) => {
+    let coordinates = { lat: 0, lng: 0 };
+    this.props.Memorials.forEach((memorial) => {
+      if (memorial.Id === memorialId) {
+        coordinates = getCoordinatesOfMemorial(memorial);
+        this.setState({
+          lastClickedCoordinates: coordinates,
+        });
+      }
+    });
+    this.props.onSidebarClick(coordinates);
+  };
+
+  render() {
+    return (
+      <div className={styles.Sidebar}>
+        <input
+          onChange={(event) => this.props.searchHandler(event.target.value)}
+          className={styles.SearchInput}
+          type="text"
+          placeholder="What are you looking for?"
+        />
+        <MapCenterContext.Provider value={this.state.lastClickedCoordinates}>
+          <div className={styles.ScrollMenu}>
+            {this.props.Memorials.map((memorial) => {
+              return (
+                <Memorial
+                  key={memorial.Id}
+                  Id={memorial.Id}
+                  Name={memorial.Name}
+                  Type={memorial.Type}
+                  hide={memorial.hideIcon}
+                  onSidebarClick={this.onSidebarClick}
+                />
+              );
+            })}
+          </div>
+        </MapCenterContext.Provider>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default sidebar;
+export default Sidebar;
