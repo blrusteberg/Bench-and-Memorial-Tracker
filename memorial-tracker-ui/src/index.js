8e82f2de2941dbe5554fe36aed3763a8f4642a8a
Swade Howard <swade.howard@gmail.com>
2020-09-23 20:40:59 -0500

    DEPLOYMENT FIX

--- memorial-tracker-ui/src/index.js
+++ memorial-tracker-ui/src/index.js
@@ -4,14 +4,6 @@ import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
-var express = require("express");
-var server = express();
-var options = {
-  index: "index.html",
-};
-
-server.use("/", express.static("/home/site/wwwroot", options));
-server.listen(provess.env.PORT);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
