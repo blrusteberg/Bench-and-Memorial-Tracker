4ee8e67907cad7d4420c4f9469d527efd94d77fa
Swade Howard <swade.howard@gmail.com>
2020-09-23 19:53:35 -0500

    PREP FOR DEPLOYMENT

--- memorial-tracker-ui/src/index.js
+++ memorial-tracker-ui/src/index.js
@@ -4,6 +4,15 @@ import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
+var express = require("express");
+var server = express();
+var options = {
+  index: "index.html",
+};
+
+server.use("/", express.static("/home/site/wwwroot", options));
+server.listen(provess.env.PORT);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
