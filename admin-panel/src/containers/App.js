d8703be3c882928d9133677571bb915345a6b940
Swade Howard <swade.howard@gmail.com>
2020-10-01 17:23:15 -0500

    merge conflix fix

--- admin-panel/src/containers/App.js
+++ admin-panel/src/containers/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router, Switch, Route, BrowserRouter } from "react-router-dom";
 import { CaretRightOutlined } from "@ant-design/icons";
+import dotenv from "dotenv";
 import "antd/dist/antd.css";
 
 import styles from "./App.module.css";
