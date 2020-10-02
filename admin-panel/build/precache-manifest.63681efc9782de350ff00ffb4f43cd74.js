7eaa0981d4655efa9a2503d0bb6f2d95738e09a6
Swade Howard <swade.howard@gmail.com>
2020-10-02 18:25:15 -0500

    cleaned up repo, removed old code, fixed .gitignore

---
+++ admin-panel/build/precache-manifest.63681efc9782de350ff00ffb4f43cd74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eee7809d3a8494c6b0b851e546b1517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "003d7842273fff474865",
+    "url": "/static/css/main.c95fdb3b.chunk.css"
+  },
+  {
+    "revision": "a8e21edef678792e9b38",
+    "url": "/static/js/2.1ff718ff.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.1ff718ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003d7842273fff474865",
+    "url": "/static/js/main.97478967.chunk.js"
+  },
+  {
+    "revision": "3cf77bfcb77e97c8cd0b",
+    "url": "/static/js/runtime-main.cbdf1f68.js"
+  },
+  {
+    "revision": "61edc82ac5fd71221a581887f7b6c2dc",
+    "url": "/static/media/deleteAttribute.61edc82a.png"
+  }
+]);
---
+++ memorial-tracker-ui/build/precache-manifest.1294e2f4ec5b7d0a822d854f110e5072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6cade0f184f4a3088d8875c1d5720e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79fb37a2d49840b88e7b",
+    "url": "/static/css/main.31514913.chunk.css"
+  },
+  {
+    "revision": "c14b149f75d87dad1919",
+    "url": "/static/js/2.1e6c6631.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.1e6c6631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fb37a2d49840b88e7b",
+    "url": "/static/js/main.27a6e1b0.chunk.js"
+  },
+  {
+    "revision": "a306ad78441cba92efcd",
+    "url": "/static/js/runtime-main.fa3bd077.js"
+  }
+]);
